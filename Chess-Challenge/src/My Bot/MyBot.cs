e314676f3281c5e65974827b41399466f915700e
TGazel <44865341+TGazel@users.noreply.github.com>
2023-08-03 16:34:03 +0200

    framework danger tracking

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,26 @@
 ﻿using ChessChallenge.API;
+using System;
 
 public class MyBot : IChessBot
 {
+    bool BotInit;
+    int[,] WhiteDangerLvl;
+    int[,] BlackDangerLvl;
+
+
     public Move Think(Board board, Timer timer)
     {
+        if (!this.BotInit) //initialisation with precalculated values at start
+        {
+            this.WhiteDangerLvl = new int[8,8];
+            this.BlackDangerLvl = new int[8,8];
+        }
+
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move move = moves[0];
+        //board.MakeMove(move);
+        //board.UndoMove(move);
+        return move;
     }
+    
 }
