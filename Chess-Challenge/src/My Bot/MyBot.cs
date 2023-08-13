16cefb500389b54b6b30f9303318f94db393963a
TGazel <44865341+TGazel@users.noreply.github.com>
2023-08-14 01:20:43 +0200

    Setup metric

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -3,23 +3,50 @@ using System;
 
 public class MyBot : IChessBot
 {
-    bool BotInit;
-    int[,] WhiteDangerLvl;
-    int[,] BlackDangerLvl;
+    // array (64) of danger values
+    int[] WhiteDangerLvl; 
+    int[] BlackDangerLvl;
 
+    public MyBot() //initialisation with precalculated values at start (temporary 0)
+    {
+        this.WhiteDangerLvl = new int[64];
+        this.BlackDangerLvl = new int[64];
+    }
+
+    public MyBot(int[] wdl, int[] bdl) //initialisation with given values for testing purposes
+    {
+        if (wdl.GetLength(0) != 64 || bdl.GetLength(0) != 64) //TODO: replace by proper unittest
+        {
+        this.WhiteDangerLvl = new int[64];
+        this.BlackDangerLvl = new int[64];
+        }
+        this.WhiteDangerLvl = wdl;
+        this.BlackDangerLvl = bdl;
+    }
 
     public Move Think(Board board, Timer timer)
     {
-        if (!this.BotInit) //initialisation with precalculated values at start
-        {
-            this.WhiteDangerLvl = new int[8,8];
-            this.BlackDangerLvl = new int[8,8];
-        }
-
         Move[] moves = board.GetLegalMoves();
         Move move = moves[0];
-        //board.MakeMove(move);
-        //board.UndoMove(move);
+        float metric = 1.0F;
+
+
+        for (int nbm = 0; nbm < moves.GetLength(0); nbm ++)
+        {
+            Move testmove = moves[nbm];
+            float testmetric= 1.0F;
+            board.MakeMove(testmove);
+            //TODO: update dangerlvls & compute testmetric for decision making
+            board.UndoMove(testmove);
+
+            if (testmetric<metric)
+            {
+                metric = testmetric;
+                move = testmove;
+            }
+
+        }
+        
         return move;
     }
     
